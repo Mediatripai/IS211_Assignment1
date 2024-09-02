ee193122eb04734786ccee10ccca254e3e9b4d96
Mediatripai <92997548+Mediatripai@users.noreply.github.com>
2024-09-01 23:07:13 -0400

    Update assignment1_part2.py

--- assignment1_part2.py
+++ assignment1_part2.py
@@ -1,11 +1,17 @@
+# assignment1_part2.py
 
 class Book:
-    def __init__(self, author, title):
-        pass
-    
+    def __init__(self, author="", title=""):
+        self.author = author
+        self.title = title
+
     def display(self):
-        pass
+        print(f"{self.title}, written by {self.author}")
 
 
 if __name__ == "__main__":
-    pass
+    book1 = Book("J. K. Rowling", "Harry Potter and the Goblet of Fire")
+    book2 = Book("Walter Scott", "Ivanhoe: A Romance")
+
+    book1.display()
+    book2.display()
